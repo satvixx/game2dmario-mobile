ea370f9fee7d4f9dc517cd45ac2b81dd4c120711
Satvi Gupta <125191844+satvixx@users.noreply.github.com>
2026-08-20 20:29:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
   },
   
   tanstackStart: {
-  
+  spa: {
+      enabled: true,
+    },
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { entry: "server" },
